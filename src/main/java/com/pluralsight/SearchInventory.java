6e1ffc9460e8c1653edd4c84cc89546523aaa1fb
RickelviC <Cruzrickelvi@gmail.com>
2026-04-22 12:11:18 -0400

    added reading the file also shows the info

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,10 +1,15 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchInventory {
+    private static final String CVS_File = "inventory.csv";
+
     public static void main(String[] args) {
-        ArrayList<Product> inventory = getInventory();
+        List<Product> inventory = getInventory();
 
         for (Product product : inventory) {
             System.out.println(product);
@@ -12,10 +17,30 @@ public class SearchInventory {
 
     }
 
-    public static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory = new ArrayList<>();
+    public static List<Product> getInventory() {
+        List<Product> inventory = new ArrayList<>();
 
+        String line;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(CVS_File));
+
+            while ((line = reader.readLine()) != null) {
+                int i = 0;
+                String[] divider = line.split("\\|");
+                int id = Integer.parseInt(divider[0]);
+                String name = divider[1];
+                double price = Double.parseDouble(divider[2]);
+
+                inventory.add(i,new Product(id,name,price));
+                i++;
+            }
+
+        } catch (Exception ex) {
+            System.err.println("Something went wrong");
+        }
         // Write your code here
+
 
         return inventory;
     }
